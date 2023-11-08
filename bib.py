72bf438616a04f585a2216ac94dcdaf06c4b972e
carlo e rafa <carlosrafaelgit@gmail.com>
2023-11-08 09:31:57 -0300

    avaliado

--- bib.py
+++ bib.py
@@ -10,8 +10,10 @@ dicio = {
 
 #exer 1
 def PreencherTable(d: dict, t: list) -> None:
+    i = 0
+    cont = 0
     while True:
-        name = input("Nome: ")
+        name = input("\nNome: ")
         if  name == '.':
             break
         else:
@@ -21,87 +23,121 @@ def PreencherTable(d: dict, t: list) -> None:
             d['email'] = input("seu email: ")
             d['telefone'] = input("Seu Telefone: ")
             t.append(d.copy())
+    for i in range(len(t)):
+        print(f"\nRegistro numero {cont}\n")
+        print(f"Nome: {t[i]['nome']}")
+        print(f"Idade: {t[i]['idade']}")
+        print(f"Cpf: {t[i]['cpf']}")
+        print(f"Email: {t[i]['email']}")
+        print(f"Telefone: {t[i]['telefone']}")
+        cont += 1
 
 #exer 2
 def exibRegis(t: list, kei: int) -> None:
     i = 0
-    
     for i in range(len(t)):
         if i == kei and kei >= 0:
-            print(f"{t[i]}")
+            print(f"\nNome: {t[i]['nome']}")
+            print(f"Idade: {t[i]['idade']}")
+            print(f"Cpf: {t[i]['cpf']}")
+            print(f"Email: {t[i]['email']}")
+            break
         else:
             i += 1
 
 #exer 3
 def exibTudo(t: list) -> None:
-    if (t not in None):
-        for i in range(len(t)):
-            print(f"Nome: {t[i]['nome']}")
-            print(f"Idade: {t[i]['idade']}")
-            print(f"Cpf: {t[i]['cpf']}")
-            print(f"Email: {t[i]['email']}")
-    else:
-        print("Você ainda não fez o cadastro!")
+    for i in range(len(t)):
+        print(f"\nNome: {t[i]['nome']}")
+        print(f"Idade: {t[i]['idade']}")
+        print(f"Cpf: {t[i]['cpf']}")
+        print(f"Email: {t[i]['email']}")
 
 #Exer 4
 def exibir_posicao(t: list, i: int) -> None:
     if i < len(t) and i >= 0:
-        print(f"Nome...:{t[i]['nome']}") 
+        print(f"\nNome...:{t[i]['nome']}") 
         print(f"E-mail.:{t[i]['email']}") 
     else:
         print("inexistente")
 
 #Exer 5
 def consultNome (t: list, n) -> None:
-    for i in range(len(t)):
-        if (n == t[i]['nome']):
-            print(f"Nome: {t[i]['nome']}")
-            print(f"E-mail: {t[i]['email']}")
-        else:
-            print("Inexistente")
+    while True:
+        for i in range(len(t)):
+            if (t[i]['nome'] == n.lower()):
+                print(f"\nNome: {t[i]['nome']}")
+                print(f"E-mail: {t[i]['email']}")
+            else:
+                print("Inexistente")
+        resp = input("Deseja continuar a consultar? Digite [S]im ou [N]ão: ")
+        while resp not in ['s', 'n', 'S', 'N']:
+            resp = input("Deseja continuar consultando? Digite [S]im ou [N]ão: ")
+        if (resp.lower() == 's'):
+            continue
+        elif (resp.lower() == 'n'):
+            break
+            
+    
 
 # Exer 6
 def consultado(t: list, n, d: dict) -> None:
-    for i in range(len(t)):
-        if (n == t[i]['nome']):
-            print(f"Nome: {t[i]['nome']}")
-            print(f"E-mail: {t[i]['email']}")
-            print("\n ESTE REGISTRO JÁ EXISTE!! NÃO É POSSIVEL CADASTRAR")
-        else:
-            d['nome'] = n
-            d['email'] = input("E-mail: ")
-            d['idade'] = input("Idade: ")
-            d['cpf'] = input("Cpf: ")
-            d['telefone'] = input("Telefone: ")
-            t.append(d)
-             
+    while True:
+        for i in range(len(t)):
+            if (n.lower() == t[i]['nome']):
+                print(f"\nNome: {t[i]['nome']}")
+                print(f"E-mail: {t[i]['email']}")
+                print("\nESTE REGISTRO JÁ EXISTE!! NÃO É POSSIVEL CADASTRAR")
+            else:
+                d['nome'] = n
+                d['email'] = input("E-mail: ")
+                d['idade'] = input("Idade: ")
+                d['cpf'] = input("Cpf: ")
+                d['telefone'] = input("Telefone: ")
+                t.append(d)
+        resp = input("Deseja continuar a consultar? Digite [S]im ou [N]ão: ")
+        while resp not in ['s', 'n', 'S', 'N']:
+            resp = input("ERROR! Digite [S]im ou [N]ão: ")
+        if (resp.lower() == 's'):
+            continue
+        elif (resp.lower() == 'n'):
+            break         
 
 #Exer 7
 def editar(t: list, id: int) -> None:
-    for i in range(len(t)):
-        if (id == t[i]['cpf']):
-            print(f"Cpf: {t[i]['cpf']}")
-            print(f"Nome: {t[i]['nome']}")
-            print(f"Telefone: {t[i]['telefone']}")
-            print(f"E-mail: {t[i]['email']}")
-            mod = input("\n Digite qual você quer modificar: ")
+    while True:
+        for i in range(len(t)):
+            if (id.lower() == t[i]['nome']):
+                print(f"\nCpf: {t[i]['cpf']}")
+                print(f"Nome: {t[i]['nome']}")
+                print(f"Telefone: {t[i]['telefone']}")
+                print(f"E-mail: {t[i]['email']}")
+                mod = input("\n Digite qual key você quer modificar: ")
 
-            if (mod == 'nome'):
-                t[i]['nome'] = input("Digite outro nome: ")
-            elif(mod == 'telefone'):
-                t[i]['telefone'] = input("Digite outro telefone: ")
-            elif(mod == 'email'):
-                t[i]['email'] = input("Digite outro email: ")
+                if (mod == 'nome'):
+                    t[i]['nome'] = input("Digite outro nome: ")
+                elif(mod == 'telefone'):
+                    t[i]['telefone'] = input("Digite outro telefone: ")
+                elif(mod == 'email'):
+                    t[i]['email'] = input("Digite outro email: ")
+
+        resp = input("Deseja continuar editando? Digite [S]im ou [N]ão: ")
+        while resp not in ['s', 'n', 'S', 'N']:
+            resp = input("ERROR! Digite [S]im ou [N]ão: ")
+        if (resp.lower() == 's'):
+            continue
+        elif (resp.lower() == 'n'):
+            break   
 
 #Exer 8
 def excluir(t: list, id: int) -> None:
     for i in range(len(t)):
-        if (id == t[i]['cpf']):
-            print(f"Cpf: {t[i]['cpf']}")
+        if (id == t[i]['nome']):
+            print(f"\nCpf: {t[i]['cpf']}")
             print(f"Nome: {t[i]['nome']}")
             print(f"Telefone: {t[i]['telefone']}")
             print(f"E-mail: {t[i]['email']}")
-            exc = input("\n Confirma a exclusão? Digite [S]im ou [N]ão: ")
+            exc = input("\n Confirma a exclusão do registro? Digite [S]im ou [N]ão: ")
 
             if (exc.lower() == 's'):
                 t[i].clear()
--- tabelaAula.py
+++ tabelaAula.py
@@ -1,5 +1,5 @@
-
 from bib import *
+import os
 '''
 tabela = list()
 # forma 1
@@ -42,11 +42,11 @@ while True:
     print("""
         0 - Sair
         1 - Cadastrar 
-        2 - Listar
-        3 - Cadastrar multiplos (até que seja digitado.)
-        4 - Exibir um específico
+        2 - Listar um especifico
+        3 - Listar Todos
+        4 - Exibir a posição da key
         5 - Consultar por nome (ou outra chave unica)
-        6 - Cadastrar consultando
+        6 - Consultar cadastro
         7 - Editar 
         8 - Excluir
     """)
@@ -57,30 +57,30 @@ while True:
             PreencherTable(dicio, table)
 
         case 2:
-            kei = int(input("\n Digite qual key deseja ver: "))
+            kei = int(input("\nDigite qual key deseja ver: "))
             exibRegis(table, kei)
 
         case 3:
             exibTudo(table)
 
         case 4:
-            ind = int(input("\n Posição: "))
+            ind = int(input("\nPosição: "))
             exibir_posicao(table, ind)
         
         case 5:
-            name = input("\n Digite seu nome: ")
+            name = input("\nDigite seu nome: ")
             consultNome(table, name)
         
         case 6:
-            name = input("\n Nome: ")
+            name = input("\nNome: ")
             consultado(table, name, dicio)
 
         case 7:
-            id = input("Cpf: ")
+            id = input("Digite seu nome para editar o registro: ")
             editar(table, id)
 
         case 8:
-            id = input("CPF: ")
+            id = input("Digite o nome para excluir o registro: ")
             excluir(table, id)
 
         case 0:
